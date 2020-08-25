335df8d3103d4461aa5a6c935c7b0f8da89c3e58
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2020-08-25 19:32:07 +0200

    Finished

--- app/src/main/java/com/example/root_detection/MainActivity.java
+++ app/src/main/java/com/example/root_detection/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.root_detection;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String DEVICE_IS_ROOTED = "Your device is ROOTED";
+    private static final String DEVICE_IS_NOT_ROOTED = "Your device is not ROOTED";
+
+    private TextView rootedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rootedTextView = findViewById(R.id.rootedTextView);
+        if(RootDetectionUnit.isDeviceRooted()){
+            rootedTextView.setText(DEVICE_IS_ROOTED);
+        }else{
+            rootedTextView.setText(DEVICE_IS_NOT_ROOTED);
+        }
     }
 }
--- app/src/main/java/com/example/root_detection/RootDetectionUnit.java
+++ app/src/main/java/com/example/root_detection/RootDetectionUnit.java
@@ -1,5 +1,46 @@
 package com.example.root_detection;
 
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RootDetectionUnit {
-    
+    public static boolean isDeviceRooted(){
+        return checkMethod1() || checkMethod2() || checkMethod3();
+    }
+
+    private static boolean checkMethod1(){
+        String tags = Build.TAGS;
+        return tags != null && tags.contains("test-keys");
+    }
+
+    private static boolean checkMethod2(){
+        String[] paths = {"/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"};
+        for(String path : paths){
+            if(new File(path).exists()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkMethod3(){
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[] {"/system/xbin/which", "su"});
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            if(reader.readLine() != null) return true;
+
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            if(process != null) process.destroy();
+        }
+    }
 }
